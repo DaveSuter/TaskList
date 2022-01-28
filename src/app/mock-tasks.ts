22da95a7b90c9a7aa16b2d68dfe3642fcf5ffd3d
DaveSuter <daveysuter@gmail.com>
2022-01-28 19:33:39 -0300

    commit de task list

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,29 @@
+import { Task } from "./Task"
+export const TASKS: Task[] = [
+    {
+        id:1,
+        text: "Terminar la tarea de Angular",
+        day: 'Enero 27 a las 19:00hs',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Hacer las compras",
+        day: 'Enero 27 a las 19:30hs',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Probar Git Pages",
+        day: 'Enero 27 a las 20:00hs',
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "Mostrar proyecto",
+        day: 'Enero 27 a las 21:00hs',
+        reminder: false
+    }
+
+
+]
